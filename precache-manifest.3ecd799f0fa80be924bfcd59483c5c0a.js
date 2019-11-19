52a191c4a0dc779226883fe103508052668a8d88
Andrew Mowe <andrewmowe@gmail.com>
2019-11-19 17:04:13 -0500

    Updates

---
+++ precache-manifest.3ecd799f0fa80be924bfcd59483c5c0a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "525887bfd229666e3dbcef8ba366ae9a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a23c676c43c5da54aee9",
+    "url": "/monsters-rolodex/static/css/main.5a866f80.chunk.css"
+  },
+  {
+    "revision": "6ef49256656a9eb2de46",
+    "url": "/monsters-rolodex/static/js/2.8629efad.chunk.js"
+  },
+  {
+    "revision": "a23c676c43c5da54aee9",
+    "url": "/monsters-rolodex/static/js/main.adfc92b9.chunk.js"
+  },
+  {
+    "revision": "c7f991e6cb4746d6bd30",
+    "url": "/monsters-rolodex/static/js/runtime~main.205b164d.js"
+  }
+]);
